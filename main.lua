e55a2e8833ed3bc4207177d9949d5277b14e7d29
antonioN313 <antoniocarlosfilho2003@gmail.com>
2024-02-02 21:49:17 -0300

    feature: Paralax update

--- main.lua
+++ main.lua
@@ -27,7 +27,15 @@ VIRTUAL_HEIGHT = 288
 
 -- images we load into memory from files to later draw onto the screen
 local background = love.graphics.newImage('/images/background.png')
+local backgroundScroll = 0
+
 local ground = love.graphics.newImage('/images/ground.png')
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
 
 function love.load()
     -- initialize our nearest-neighbor filter
@@ -48,6 +56,10 @@ function love.resize(w, h)
     push:resize(w, h)
 end
 
+function love.update(dt)
+	backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+	groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+end
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
@@ -58,10 +70,10 @@ function love.draw()
     push:start()
     
     -- draw the background starting at top left (0, 0)
-    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(background, -backgroundScroll, 0)
 
     -- draw the ground on top of the background, toward the bottom of the screen
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
     
     push:finish()
 end
